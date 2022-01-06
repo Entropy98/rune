a03f50d741a892f721b3d4c3c1bacfc93f093618
Harper <harperweigle@gmail.com>
2022-01-06 14:39:27 -0800

    correctly displays rune and checks mouse position. Need to remove border from ovals for correct stage colors

--- python/runes.py
+++ python/runes.py
@@ -1,20 +1,51 @@
 import math
 
+def calcStageOneOffset(s, fibCalc, x=1, y=0, i=1):
+    if(i > s):
+        return (x,y)
+    return calcStageOneOffset(s,fibCalc,fibCalc(i),fibCalc(i)-1,i+1)
+
+def calcStageTwoOffset(s, fibCalc, x=1, y=1, i=1):
+    if(i > s):
+        return (x,y)
+    return calcStageTwoOffset(s,fibCalc,fibCalc(i),y+fibCalc(i-1),i+1)
+
+def calcStageThreeOffset(s,fibCalc,y=1,i=1):
+    if(i > s):
+        return y
+    return calcStageThreeOffset(s,fibCalc,y+fibCalc(i-1),i+1)
+
+def calcStageSixOffset(s,fibCalc,y=7,i=1):
+    if(i > s):
+        return y
+    return calcStageSixOffset(s,fibCalc,y+2*fibCalc(i),i+1)
+
 class Fire():
-    def stageOne(x,s):
-        return math.sqrt(s**2-(x-1)**2)
+    def stageOneY(x,fib,s,fibCalc):
+        offset = calcStageOneOffset(s, fibCalc)
+        return math.sqrt(abs(fib)**2-(abs(x)-offset[0])**2)-offset[1]
 
-    def stageTwo(x,s):
-        return math.sqrt(s**2-(x-1)**2)-1
+    def stageOneX(y,fib,s,fibCalc):
+        offset = calcStageOneOffset(s, fibCalc)
+        return math.sqrt(abs(fib)**2-(abs(y)-offset[1])**2)-offset[0]
 
-    def stageThree(x,s):
-        return -1*math.sqrt(s**2-x^2)-1
+    def stageTwoY(x,fib,s,fibCalc):
+        offset = calcStageTwoOffset(s, fibCalc)
+        return math.sqrt(abs(fib)**2-(abs(x)-offset[0])**2)-offset[1]
 
-    def stageFour(x,s):
-        return -1*math.sqrt(s**2-x^2)+1
+    def stageTwoX(y,fib,s,fibCalc):
+        offset = calcStageTwoOffset(s, fibCalc)
+        return math.sqrt(abs(fib)**2-(abs(y)-offset[1])**2)-offset[0]
 
-    def stageFive(x,s):
-        return math.sqrt(s**2-x^2)+1
+    def stageThreeY(x,fib,s,fibCalc):
+        return -1*math.sqrt(abs(fib)**2-abs(x)**2)-calcStageThreeOffset(s,fibCalc)
 
-    def stageSix(x,s):
-        return -1*math.sqrt(s**2-x^2)+7
+    def stageFourY(x,fib):
+        return -1*math.sqrt(abs(fib)**2-abs(x)**2)+1
+
+    def stageFiveY(x,fib):
+        return math.sqrt(abs(fib)**2-abs(x)**2)+1
+
+    def stageSixY(x,fib,s,fibCalc):
+        return -1*math.sqrt(abs(fib)**2-abs(x)**2)+calcStageSixOffset(s,fibCalc)
+
--- python/runetracetest.py
+++ python/runetracetest.py
@@ -2,53 +2,100 @@ import sys
 import tkinter as tk
 
 from runes import *
+from helpers import *
 
 CANVASWIDTH = 800
 CANVASHEIGHT = 400
 RESOLUTION = 100
+WIGGLE = 20
 
-def App(tk.Tk):
+class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Rune Trace Test")
         self.canvas = tk.Canvas(self, width=CANVASWIDTH, height=CANVASHEIGHT)
-        self.scale = 1
+        self.canvas.pack()
+        self.scale = 7
+        self.status = tk.StringVar()
+        self.status.set("None")
+        self.statusLabel = tk.Label(self.canvas, textvariable=self.status)
+
+        self.canvas.bind('<Motion>', self.checkCursor)
 
     def update(self):
         self.canvas.delete("all")
+        self.drawFireRune()
+        self.statusLabel.place(x=1,y=1)
+        self.canvas.pack()
         self.canvas.after(1000, self.update)
+
+    def checkCursor(self, e):
+        rune_start = self.zeroToTrueCoord(0,
+                Fire.stageOneY(0,self.nthFib(self.scale),self.scale, self.nthFib))
+        if(distance(e.x,e.y,rune_start[0],rune_start[1]) < WIGGLE):
+            self.status.set("Stage One")
+        else:
+            if(self.status.get() == "Stage One"):
+                zeroedCursor = self.trueToZeroCoord(e.x,e.y)
+                runeY = Fire.stageOneY(zeroedCursor[0],self.nthFib(self.scale),
+                        self.scale, self.nthFib)
+                runeX = Fire.stageOneX(zeroedCursor[1],self.nthFib(self.scale),
+                        self.scale, self.nthFib)
+                runeMP = midpoint(runeX, e.y, e.x, runeY)
+                if(distance(runeMP[0],runeMP[1],e.x,e.y) > WIGGLE):
+                    self.status.set("None")
 
     def drawFireRune(self):
         y=0
-        fib1 = self.scale
-        fib2 = fib1+self.scale
+        fib1 = self.nthFib(self.scale)
+        fib2 = self.nthFib(self.scale+1)
         fib3 = fib1+fib2
-        for x in range(0,1,1/RESOLUTION):
-            y=Fire.stageOne(x,fib1)
-            coords=self.zeroCoord(x,y)
-            self.canvas.create_oval(coords[0].coords[1].coords[0],coords[1],
-                    fill="black",width=2)
-        for x in range(1,3,1/RESOLUTION):
-            y=Fire.stageTwo(x,fib2)
-            coords=self.zeroCoord(x,y)
-            self.canvas.create_oval(coords[0].coords[1].coords[0],coords[1],
-                    fill="black",width=2)
-        for x in range(0,3,1/RESOLUTION):
-            y=Fire.stageTwo(x,fib3)
-            coords=self.zeroCoord(x,y)
-            self.canvas.create_oval(coords[0].coords[1].coords[0],coords[1],
-                    fill="black",width=2)
+        for x in range(0,fib1*RESOLUTION,1):
+            y=Fire.stageOneY(x/RESOLUTION,fib1,self.scale,self.nthFib)
+            coords=self.zeroToTrueCoord(x/RESOLUTION,y)
+            self.canvas.create_oval(coords[0],coords[1],coords[0],coords[1],
+                    fill="blue",width=2)
+        for x in range(fib1*RESOLUTION,fib3*RESOLUTION,1):
+            y=Fire.stageTwoY(x/RESOLUTION,fib2,self.scale,self.nthFib)
+            coords=self.zeroToTrueCoord(x/RESOLUTION,y)
+            self.canvas.create_oval(coords[0],coords[1],coords[0],coords[1],
+                    fill="red",width=2)
+        for x in range(0,fib3*RESOLUTION,1):
+            y=Fire.stageThreeY(x/RESOLUTION,fib3,self.scale,self.nthFib)
+            coords=self.zeroToTrueCoord(x/RESOLUTION,y)
+            self.canvas.create_oval(coords[0],coords[1],coords[0],coords[1],
+                    fill="blue",width=2)
+        fib4 = fib2+fib3
+        for x in range(-1*fib4*RESOLUTION,0,1):
+            y=Fire.stageFourY(x/RESOLUTION,fib4)
+            coords=self.zeroToTrueCoord(x/RESOLUTION,y)
+            self.canvas.create_oval(coords[0],coords[1],coords[0],coords[1],
+                    fill="red",width=2)
+        for x in range(-1*fib4*RESOLUTION,0,1):
+            y=Fire.stageFiveY(x/RESOLUTION,fib4)
+            coords=self.zeroToTrueCoord(x/RESOLUTION,y)
+            self.canvas.create_oval(coords[0],coords[1],coords[0],coords[1],
+                    fill="blue",width=2)
+        for x in range(0,fib1*RESOLUTION,1):
+            y=Fire.stageSixY(x/RESOLUTION,fib1,self.scale, self.nthFib)
+            coords=self.zeroToTrueCoord(x/RESOLUTION,y)
+            self.canvas.create_oval(coords[0],coords[1],coords[0],coords[1],
+                    fill="red",width=2)
 
-    def zeroCoord(x,y):
+    def zeroToTrueCoord(self, x, y):
         if(abs(x) > CANVASWIDTH/2 or abs(y)>CANVASHEIGHT/2):
             print("Error: coordinate out of bounds")
-            return
-        return (CANVASWIDTH/2+x,-1*(CANVASHEIGHT/2+y))
+        return (CANVASWIDTH/2+x,CANVASHEIGHT-(CANVASHEIGHT/2+y))
 
-    def nthFib(n, fib1=1, fib2=1, i=0):
+    def trueToZeroCoord(self, x, y):
+        if(x < 0 or x > CANVASWIDTH or y < 0 or y > CANVASHEIGHT):
+            print("Error: coordinate out of bounds")
+        return (x-CANVASWIDTH/2,y-(CANVASHEIGHT-CANVASHEIGHT/2))
+
+    def nthFib(self, n, fib1=1, fib2=1, i=0):
         if(i >= n):
             return fib2
-        self.nthFib(n, fib2, fib1+fib2, i+1)
+        return self.nthFib(n, fib2, fib1+fib2, i+1)
 
 def main():
     app = App()
